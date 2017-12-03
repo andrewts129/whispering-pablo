2b8d4127164cfa446b3a753672949598d61e17f6
Andrew Smith <andrewts129@gmail.com>
2017-12-02 21:34:23 -0500

    set to use postgres for messages

--- app/controllers/api/ApiController.scala
+++ app/controllers/api/ApiController.scala
@@ -3,28 +3,20 @@ package controllers.api
 import java.util.UUID
 import javax.inject._
 
-import play.api.db.Database
 import play.api.libs.json.{JsValue, Json}
 import play.api.mvc._
+import services.MessageFetcher
 
 @Singleton
-class ApiController @Inject()(cc: ControllerComponents, db: Database) extends AbstractController(cc) {
-  private val messageFetcher = new services.MessageFetcher()
+class ApiController @Inject()(cc: ControllerComponents, messageFetcher: MessageFetcher) extends AbstractController(cc) {
 
   def postMessage(request: String) = Action {
     val requestId:String = UUID.randomUUID().toString
 
-    val responseMessage: String = messageFetcher.message
-    val responseId: String = messageFetcher.id
+    val(responseId, responseMessage) = messageFetcher.getLastMessage()
+    messageFetcher.postMessage(requestId, request)
 
-    messageFetcher.message = request
-    messageFetcher.id = requestId
-
-    val conn = db.getConnection()
-    conn.createStatement().execute("INSERT INTO messages(id, text, time) VALUES ('testId', 'testMessage', now())")
-    conn.close()
-
-    val response: JsValue = Json.obj("request" -> Json.obj("message" -> request, "id" -> requestId), "response" -> Json.obj("message" -> responseMessage, "id" -> responseId))
+    val response: JsValue = Json.obj("request" -> Json.obj("id" -> requestId, "message" -> request), "response" -> Json.obj("id" -> responseId, "message" -> responseMessage))
     Ok(response)
   }
 
--- app/services/MessageFetcher.scala
+++ app/services/MessageFetcher.scala
@@ -1,10 +1,53 @@
 package services
 
+import java.sql.{ResultSet, Statement}
 import java.util.UUID
+import javax.inject.Inject
 
-import akka.http.scaladsl.model.headers.CacheDirectives.public
+import play.api.db.Database
 
-class MessageFetcher {
+class MessageFetcher @Inject()(db: Database) {
   var message: String = "Hello World!"
   var id: String = UUID.randomUUID().toString
+
+  def getLastMessage(): (String, String) = {
+    val connection = db.getConnection()
+
+    // Default values in case something goes wrong
+    var id = "ERROR"
+    var message = "ERROR"
+
+    try {
+      val statement: Statement = connection.createStatement()
+
+      // Gets the last thing submitted to the db
+      val response: ResultSet = statement.executeQuery("SELECT id, text FROM messages WHERE id = (SELECT id FROM messages ORDER BY time DESC LIMIT 1);")
+      if (response.next()) {
+        id = response.getString("id")
+        message = response.getString("text")
+      }
+
+      // Deletes the result retrieved above
+      // TODO Combine statements
+      statement.executeUpdate("DELETE FROM messages WHERE id = '" + id + "'")
+
+    }
+    finally {
+      connection.close()
+    }
+
+    (id, message)
+  }
+
+  def postMessage(id: String, text: String): Unit = {
+    val connection = db.getConnection()
+
+    try {
+      val statement: Statement = connection.createStatement()
+      statement.executeUpdate("INSERT INTO messages(id, text, time) VALUES ('" + id + "', '" + text + "', now());")
+    }
+    finally {
+      connection.close()
+    }
+  }
 }
